8d9211a5e4ebbfe4602568085693a4ec19e2eb68
Ari Entlich <lmage11@twcny.rr.com>
2007-01-28 02:30:32 -0500

    add a BIIIG comment, make the macros actually work

--- error.h
+++ error.h
@@ -49,6 +49,88 @@
  *      so you don't have to partake in this elaborate ritual too often...
  */
 
+/* Ok, I think it's high time I had a definitive policy on what errors use what
+ * error handling method. Here's a list of the most common errors (note that
+ * most of these are not really very common, but they are relative to the
+ * REALLY obscure ones).
+ *
+ * This first group here I'm thinking will always use FATAL_ERR. This is because
+ * they would either only be returned if there was an internal problem with
+ * libterm or they are so broad that the problem would affect other programs
+ * anyway and should be fixed. Detailed analysis:
+ *
+ * EFAULT - I don't know how this is used, since programs usually just segfault
+ *          with a bad address, but whatever. If this happens, it's obviously
+ *          because of an internal error or because the compiler is screwed up.
+ * EINVAL - As far as I can tell, this would only happen if there's an internal
+ *          problem. This usually means that one of the arguments to a function
+ *          is invalid.
+ * ETXTBSY- Why would libterm ever even try to open something that could ever
+ *          in a million years be an executable file? Something is wrong if
+ *          we get this error.
+ * ENOMEM - Program should close as soon as possible, I'm not going to try to
+ *          write some sort of smart memory reclaiming mechanism.
+ * EMFILE - libterm really shouldn't open that many things, unless you're
+ *          managing a LOT of terminals with it... Also, libterm isn't
+ *          responsible for the program using it. The user should raise their
+ *          max open files per process setting.
+ * ENFILE - libterm isn't responsible for the actions of the other programs
+ *          on the system. The user should raise their max open files setting.
+ * EPERM  - This should always be fatal. It means we can't do something that
+ *          we're trying to do, and we'll never be able to do it (ex: making
+ *          device nodes).
+ *
+ * I think these should definitely not be fatal:
+ *
+ * EACCES - VERY common error. This should definitely not be fatal.
+ * ENOENT - Also a VERY common error.
+ *
+ * I think these ones should be fatal or not depending on the situation:
+ *
+ * EEXIST - A lot of the time, when we're trying to make a file and it already
+ *          exists and is the right type, this is not a bad error at all. In
+ *          other places, however, it can be bad. Thus, it depends.
+ * ENOSPC - Very broad problem. However, in the case of a file buffer,
+ *          libterm should probably not die. It should probably give a message
+ *          to the user saying that the buffer will start being truncated at
+ *          the top or is falling back to memory buffering.
+ * EROFS  - VERY broad problem. In the case of a file buffer, again libterm
+ *          should tell the user that buffering is not possible or is falling
+ *          back to memory buffering.
+ *
+ * I'm not sure about these ones. To figure out if they should be fatal or not,
+ * let's look at the sort of files libterm will likely be interacting with:
+ *   pty master/slave files:
+ *     /dev/ptmx
+ *     /dev/tty[char][num]
+ *     /dev/pty[char][num]
+ *     used by trynode (see trynode.c):
+ *       /dev/pty[num]
+ *       /tmp/pts/[num]
+ *       /tmp/pty[num]
+ *       ~/.pts/[num]
+ *       /tmp/pt/[num]
+ *       ~/.pt/[num]
+ *       more in the future?
+ *   /var/log/{u,w}tmp? (recording logins)
+ *   I'll probably use mkstemp or some such thing to make a file buffer
+ *   The shell
+ *
+ * As you can see, there isn't much to mess up here. Taking these names into
+ * account, here is my verdict for the remaining errors:
+ *
+ * ELOOP  - This error happens when too many levels of symlinks have to be
+ *          dereferenced. Is it very likely that this will happen, seeing as
+ *          most of the paths we're using are very short? No, not really.
+ *          I think this should be a fatal error.
+ * ENOTDIR- This means that some component of the path is not a directory.
+ *          Again, there isn't much chance of that happening with out limited
+ *          set of paths. Fatal.
+ * EISDIR - This means that the last component of the path is a directory and
+ *          it shouldn't be. Again, it seems very unlikely that this will
+ *          happen on a properly configured system. Fatal.
+ */
+
 /* taken from http://gcc.gnu.org/onlinedocs/gcc-4.1.1/gcc/Function-Names.html */
 #if __STDC_VERSION__ < 199901L
 # if __GNUC__ >= 2
@@ -61,9 +143,12 @@
 /* program should try to resolve or report the error
  * used for system call errors
  */
-#define FIXABLE_SYS_ERR(name, data) \
+#define FIXABLE_SYS_ERR(name, _data) \
 	do { \
-		curerr = {name, __func__, data, errno}; \
+		curerr.sys_func = name; \
+		curerr.ltm_func = __func__; \
+		curerr.data = _data; \
+		curerr.err_no = errno; \
 		if(always_dump) error_info_dump(curerr, LTM_TRUE); \
 		return -1; \
 	} while(0);
@@ -73,7 +158,10 @@
  */
 #define FIXABLE_LTM_ERR(err) \
 	do { \
-		curerr = {NULL, __func__, NULL, err}; \
+		curerr.sys_func = NULL; \
+		curerr.ltm_func = __func__; \
+		curerr.data = NULL; \
+		curerr.err_no = err; \
 		if(always_dump) error_info_dump(curerr, LTM_TRUE); \
 		errno = err; \
 		return -1; \
@@ -82,9 +170,12 @@
 /* program should not try to resolve or report the error
  * used for system call errors
  */
-#define FATAL_ERR(name, data) \
+#define FATAL_ERR(name, _data) \
 	do { \
-		curerr = {name, __func__, data, errno}; \
+		curerr.sys_func = name; \
+		curerr.ltm_func = __func__; \
+		curerr.data = _data; \
+		curerr.err_no = errno; \
 		error_info_dump(curerr, LTM_FALSE); \
 		errno = 0; \
 		return -1; \
