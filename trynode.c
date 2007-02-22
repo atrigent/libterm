a27545f6fddd458527b510e2bc18defa9fc708a1
Ari Entlich <lmage11@twcny.rr.com>
2007-02-22 02:09:06 -0500

    ok, I THINK I'm finally done writing trynode. I'm not really sure how useful it is, but here it is. Don't blame me if your system explodes because of it. Now I can get to more interesting things :).

--- trynode.c
+++ trynode.c
@@ -10,37 +10,52 @@
 char * unix98_paths[] = {
 	"/dev/pty",
 	"/tmp/pts/",
-	"/tmp/pty",
 	"~/.pts/",
 	NULL
 };
 
-char * bsdpty_ptmx_paths[] = {
+char * bsd_ptmx_paths[] = {
+	"/dev/",
 	"/tmp/pt/",
 	"~/.pt/",
 	NULL
 };
 
-char * try_create(char * orig, char type, dev_t device, char * paths[]) {
-	int i, len, created;
+/* Arguments:
+ * orig - the basename of the node file to try and create
+ * type - 'c' (for making a character device) or 'b' (for making a block device)
+ * device - device major and minor info - created using the makedev() macro
+ * paths - see full description
+ * path - the return argument
+ *
+ * So here's how this works:
+ * The function loops through each string in paths and tries appending orig to
+ * it to create the full node file path. It then tries creating the node. If it
+ * can't, it goes to the next element in the paths array.
+ *
+ * Note that the strings in the paths array can contain part of what will
+ * become the basename of the final path - orig is appended to each element of
+ * paths no questions asked.
+ *
+ * Also, if a particular element of paths ends with a /, trynode will attempt
+ * to create a directory with that name.
+ */
+int try_create(char * orig, char type, dev_t device, char * paths[], char * path) {
+	int i, created;
 	mode_t mode;
 	struct stat stat;
-	char path[PTY_PATH_LEN];
 
 	for(i = 0; paths[i]; i++) {
-		len = strlen(paths[i]);
 		mode = S_IRUSR | S_IWUSR;
+		created = LTM_TRUE;
 
-		if(paths[i][len-1] == '/') {
+		if(paths[i][strlen(paths[i])-1] == '/') {
 			if(mkdir(paths[i], mode) == -1) {
 				if(errno == EACCES ||
-				   errno == ENOENT ||
-				   errno == ENOTDIR) continue;
-				else if(errno == EEXIST);
+				   errno == ENOENT) continue;
+				else if(errno == EEXIST) created = LTM_FALSE;
 				else FATAL_ERR("mkdir", paths[i])
 			}
-
-			created = LTM_TRUE;
 		}
 		else created = LTM_FALSE;
 		
@@ -52,13 +67,49 @@ char * try_create(char * orig, char type, dev_t device, char * paths[]) {
 
 		if(mknod(path, mode, device) == -1) {
 			if(errno == EACCES ||
-			   errno == ENOENT ||
-			   errno == ENOTDIR) continue;
-			else if(errno == EEXIST && 
+			   errno == ENOENT) continue;
+			else if(errno == EEXIST) {
+				if(is_chrdev(path, device)) break;
+				else continue;
+			}
+			else {
+				/* I think it's safe to assume that there's something very
+				 * wrong if we can't delete the directory we created a split
+				 * second ago, you?
+				 */
+				if(created && rmdir(paths[i]) == -1) FATAL_ERR("rmdir", paths[i]);
+				FATAL_ERR("mknod", path)
+			}
+		}
 
+		break;
 	}
 }
-char * ptmx_try_create() {
-	char path[PTY_PATH_LEN];
 
-	
+int ptmx_try_create(char * path) {
+	return try_create("ptmx", 'c', makedev(PTMX_MAJOR, PTMX_MINOR), bsd_ptmx_paths, path);
+}
+
+int unix98_slave_try_create(u4 num, char * path) {
+	char orig[PTY_PATH_LEN];
+
+	sprintf(orig, "%u", num);
+
+	return try_create(orig, 'c', makedev(136 + num/256, num % 256), unix98_paths, path);
+}
+
+int bsd_slave_try_create(u1 chr, u1 num, char * path) {
+	char orig[PTY_PATH_LEN];
+
+	sprintf(orig, "tty%c%x", chr, num);
+
+	return try_create(orig, 'c', makedev(3, (chr >= 112 ? chr-112 : chr-86)*16 + num), bsd_ptmx_paths, path);
+}
+
+int bsd_master_try_create(u1 chr, u1 num, char * path) {
+	char orig[PTY_PATH_LEN];
+
+	sprintf(orig, "pty%c%x", chr, num);
+
+	return try_create(orig, 'c', makedev(2, (chr >= 112 ? chr-112 : chr-86)*16 + num), bsd_ptmx_paths, path);
+}
