57e71007326498b5ed92b3b612e1475e1c3d8b70
Ari Entlich <lmage11@twcny.rr.com>
2007-03-22 17:34:52 -0400

    make the BSD terminal finder function a little more easily readable by using a do while loop also make it able to actually pass the values to the outside world

--- bsd.c
+++ bsd.c
@@ -6,16 +6,7 @@
 
 #include "libterm.h"
 
-static int next_bsd_pty(char * ident, int * init) {
-	if(*init) {
-		*init = 0;
-		
-		ident[0] = 'p';
-		ident[1] = '0';
-		
-		return LTM_TRUE;
-	}
-
+static int next_bsd_pty(char * ident) {
 	if(ident[1] == 'f') {
 		if(ident[0] == 'z') ident[0] = 'a';
 		else if(ident[0] == 'e') return LTM_FALSE;
@@ -33,16 +24,15 @@ static int next_bsd_pty(char * ident, int * init) {
  * a general infrastructure for trying different
  * PTY types.
  */
-int find_unused_bsd_pty() {
-	int init = 1;
+int find_unused_bsd_pty(FILE ** master, FILE ** slave) {
 	FILE *pty_file, *tty_file;
-	char tty_path[11], pty_path[11], tty_spc[2];
-	
+	char tty_path[11], pty_path[11], tty_spc[2] = "p0";
+
 	strcpy(tty_path, "/dev/tty");
 	strcpy(pty_path, "/dev/pty");
 	pty_path[10] = tty_path[10] = 0;
 
-	while(next_bsd_pty(tty_spc, &init)) {
+	do {
 		pty_path[8] = tty_path[8] = tty_spc[0];
 		pty_path[9] = tty_path[9] = tty_spc[1];
 
@@ -55,12 +45,13 @@ int find_unused_bsd_pty() {
 			if(!tty_file)
 				printf("Could not open pty slave %s: %s\n",
 						tty_path, strerror(errno));
-			else {
-				fclose(tty_file);
-			}
-			fclose(pty_file);
+
+			*master = pty_file;
+			*slave = tty_file;
+
+			return 1;
 		}
-	}
+	} while(next_bsd_pty(tty_spc))
 
 	return 0;
 }
