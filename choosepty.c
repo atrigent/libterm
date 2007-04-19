6848352cf47e8b3d6b65648c0c809450fdaa2426
Ari Entlich <lmage11@twcny.rr.com>
2007-04-18 23:31:54 -0400

    add a file with a function that tried different pty methods and when one works it returns the result

---
+++ choosepty.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+#include "libterm.h"
+
+/* in the future, the user will be able to specify the priority */
+ptyfunc pty_func_prior[] = {
+	alloc_unix98_pty,
+	find_unused_bsd_pty,
+	alloc_func_pty,
+	NULL
+};
+
+struct ptydev * choose_pty_method() {
+	struct ptydev * pty;
+	int i, result;
+
+	pty = malloc(sizeof(struct ptydev));
+	pty->master = 0;
+
+	for(i = 0; pty_func_prior[i]; i++) {
+		result = (*pty_func_prior[i])(pty);
+
+		if(result == 0 || result == -1) continue;
+		else break;
+	}
+
+	if(!pty->master) {
+		free(pty);
+		return NULL;
+	}
+	else return pty;
+}
