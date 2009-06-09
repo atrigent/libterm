020ce5014a551e1beec10088c79505e1b067b2e6
Ari Entlich <lmage11@twcny.rr.com>
2009-06-09 16:37:03 -0400

    Reduce duplication in bootstrap.sh by making a little function for running the commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
+run() {
+	echo "Running ${@}...";
+	$@ || exit;
+}
+
 AUXDIR=`autom4te -l Autoconf -t AC_CONFIG_AUX_DIR:\\$1 configure.ac`
 if [ -n "$AUXDIR" ]; then
 	echo "Creating auxiliary build tools directory ($AUXDIR)..."
 	mkdir -p $AUXDIR
 fi
 
-echo Running aclocal...
-aclocal || exit
-
-echo Running libtoolize --copy --force...
-libtoolize --copy --force || exit
-
-echo Running autoconf...
-autoconf || exit
-
-echo Running autoheader...
-autoheader || exit
-
-echo Running automake --gnu --add-missing --copy...
-automake --gnu --add-missing --copy
+run aclocal
+run libtoolize --copy --force
+run autoconf
+run autoheader
+run automake --gnu --add-missing --copy
