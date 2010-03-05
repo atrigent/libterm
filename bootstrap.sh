fb4bb060cf1a5e3b9bf4d4d786ac55864736cc98
Ari Entlich <atrigent@ccs.neu.edu>
2010-03-05 06:42:23 -0500

    Simplify the bootstrap script a little bit...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-run() {
-	echo "Running ${@}...";
-	$@ || exit;
-}
-
-AUXDIR=`autom4te -l Autoconf -t AC_CONFIG_AUX_DIR:\\$1 configure.ac`
-if [ -n "$AUXDIR" ]; then
-	echo "Creating auxiliary build tools directory ($AUXDIR)..."
-	mkdir -p $AUXDIR
-fi
-
-run aclocal
-run libtoolize --copy --force --ltdl
-run autoconf
-run autoheader
-run automake --gnu --add-missing --copy
+autoreconf --install --force --verbose
