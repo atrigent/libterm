dc4e7ed7338d3cb0ede7244857649bfd02b648da
Ari Entlich <atrigent@ccs.neu.edu>
2010-03-05 06:27:33 -0500

    Add some (possibly preliminary) module handling code
    This adds three functions which offer approximately the same
    functionality as dlopen, dlsym, and dlclose, except that they
    do some extra libterm-specific stuff. Probably the most important
    change is that modules are kept track of via "module IDs" ("mid"s)
    instead of handles. This commit also adds the necessary global
    structures and values for keeping track of modules by ID.

--- src/module.c
+++ src/module.c
@@ -1,4 +1,9 @@
+#include <stdlib.h>
 #include <string.h>
+
+#include "libterm.h"
+#include "module.h"
+#include "idarr.h"
 
 const char *const ltm_classes[] = {
 	/* The 'libterm' class is actually a pseudoclass -
@@ -10,3 +15,78 @@ const char *const ltm_classes[] = {
 	/* 0 */ "libterm",
 	NULL
 };
+
+struct module *modules = NULL;
+int next_mid = 0;
+
+void *module_get_sym(int mid, char *sym) {
+	void *ret;
+
+	DIE_ON_INVAL_MID_PTR(mid);
+
+	LTDL_CALL_SAVERET_PTR(ret, sym, (modules[mid].handle, sym), error);
+
+error:
+	return ret;
+}
+
+int module_load(enum moduleclass class, char *basename) {
+	modulefunc registerfunc;
+	lt_dlhandle handle;
+	int ret = -1;
+	char *path;
+
+	if(class == LIBTERM)
+		LTM_ERR(EINVAL, "Modules cannot have class libterm", before_anything);
+
+	for(ret = 0; ret < next_mid; ret++)
+		if(modules[ret].allocated && modules[ret].class == class && !strcmp(modules[ret].name, basename)) {
+			modules[ret].refcount++;
+			goto before_anything;
+		}
+	
+	path = malloc(strlen(ltm_classes[class]) + 1 + strlen(basename) + 1);
+	if(!path) SYS_ERR("malloc", NULL, before_anything);
+
+	sprintf(path, "%s_%s", ltm_classes[class], basename);
+
+	LTDL_CALL_SAVERET(handle, openext, (path), after_alloc);
+
+	ret = IDARR_ID_ALLOC(modules, next_mid);
+	if(ret == -1) goto after_alloc;
+
+	modules[ret].handle = handle;
+	modules[ret].class = class;
+	modules[ret].name = strdup(basename);
+	if(!modules[ret].name) SYS_ERR("strdup", NULL, after_alloc);
+	modules[ret].refcount++;
+
+	registerfunc = module_get_sym(ret, "libterm_module_init");
+	if(!registerfunc) PASS_ERR(after_alloc);
+
+	MODULE_CALL(modules[ret].entrypoint, registerfunc, ret);
+	if(!modules[ret].entrypoint) LTM_ERR(ENODEV, path, after_alloc);
+
+after_alloc:
+	free(path);
+before_anything:
+	return ret;
+}
+
+int module_unload(int mid) {
+	int ret = 0;
+
+	DIE_ON_INVAL_MID(mid);
+
+	if(--modules[mid].refcount) return 0;
+
+	LTDL_CALL(close, (modules[mid].handle), error);
+
+	free(modules[mid].name);
+
+	if(IDARR_ID_DEALLOC(modules, next_mid, mid) == -1)
+		return -1;
+
+error:
+	return ret;
+}
--- src/module.h
+++ src/module.h
@@ -3,16 +3,64 @@
 
 #include <ltdl.h>
 
+#define DIE_ON_INVAL_MID(mid) \
+	if((mid) >= next_mid || modules[mid].allocated == 0) \
+		LTM_ERR(EINVAL, "Invalid MID", error);
+
+#define DIE_ON_INVAL_MID_PTR(mid) \
+	if((mid) >= next_mid || modules[mid].allocated == 0) \
+		LTM_ERR_PTR(EINVAL, "Invalid MID", error);
+
+#define LTDL_CALL_SAVERET(var, func, args, label) \
+	do { \
+		errno = 0; \
+		var = lt_dl##func args; \
+		if(!var) SYS_ERR("lt_dl" #func, lt_dlerror(), label); \
+	} while(0)
+
+#define LTDL_CALL_SAVERET_PTR(var, func, args, label) \
+	do { \
+		errno = 0; \
+		var = lt_dl##func args; \
+		if(!var) SYS_ERR_PTR("lt_dl" #func, lt_dlerror(), label); \
+	} while(0)
+
 #define LTDL_CALL(func, args, label) \
 	do { \
 		errno = 0; \
 		if(lt_dl##func args) SYS_ERR("lt_dl" #func, lt_dlerror(), label); \
 	} while(0)
 
+#define MODULE_CALL(ret, func, mid) \
+	do { \
+		ret = func(modules[mid].class, modules[mid].name, &modules[mid].private); \
+	} while(0)
+
 enum moduleclass {
 	LIBTERM
 };
 
+typedef void *(*modulefunc)(enum moduleclass, char *, void **);
+
+struct module {
+	char allocated;
+	int refcount;
+
+	lt_dlhandle handle;
+
+	enum moduleclass class;
+	char *name;
+
+	void *entrypoint;
+	void *private;
+};
+
+extern int module_load(enum moduleclass, char *);
+extern int module_unload(int);
+extern void *module_get_sym(int, char *);
+
 extern const char *const ltm_classes[];
+extern struct module *modules;
+extern int next_mid;
 
 #endif
