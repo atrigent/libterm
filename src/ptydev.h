db388f2016a0052d28a59026817e6d238183007c
Ari Entlich <atrigent@ccs.neu.edu>
2011-05-01 16:03:50 -0400

    This stuff hasn't been used in AGES. I can't believe it's still there.

--- src/ptydev.h
+++ src/ptydev.h
@@ -7,35 +7,6 @@ enum ptytype {
 	FUNC_PTY
 };
 
-/* From here to... */
-/* Since major is a function on SVR4, we can't use `ifndef major'.  */
-#if MAJOR_IN_MKDEV
-# include <sys/mkdev.h>
-# define HAVE_MAJOR
-#endif
-#if MAJOR_IN_SYSMACROS
-# include <sys/sysmacros.h>
-# define HAVE_MAJOR
-#endif
-#ifdef major			/* Might be defined in sys/types.h.  */
-# define HAVE_MAJOR
-#endif
-
-#ifndef HAVE_MAJOR
-# define major(dev)  (((dev) >> 8) & 0xff)
-# define minor(dev)  ((dev) & 0xff)
-# define makedev(maj, min)  (((maj) << 8) | (min))
-#endif
-#undef HAVE_MAJOR
-
-#if ! defined makedev && defined mkdev
-# define makedev(maj, min)  mkdev (maj, min)
-#endif
-/* ...here is taken from the GNU Coreutils project. URL:
- * http://cvs.savannah.gnu.org/viewcvs/coreutils/src/system.h?rev=1.159&root=coreutils&view=markup
- * Lines 72 to 94 
- */
-
 struct ptydev {
 	enum ptytype type;
 	FILE *master;
